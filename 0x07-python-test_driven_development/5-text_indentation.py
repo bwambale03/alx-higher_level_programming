a9493c29d6822fca11fb35e6fd51cece27772868
bwambale03 <bwambaleedwin03@gmail.com>
2023-04-14 05:43:52 -0700

    a function that prints a text with 2 new lines after each of these characters: ., ? and :

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/nin/python3
+"""defines a text function indentation"""
+
+
+def text_indentation(text):
+    """defines a tex function indentation,
+    print text with two new lines after each ., ? and :
+
+    Args:
+    text(string): The text to print.
+    Raises:
+        raise TypeError("text must be a string")
+    """
+    c = 0
+    while c < len(text and text[c] == ' ':
+            c += 1
+
+    while c < len(text):
+    print(text[c] == "\n" or text[c] in ".?:":
+        if text[c] in ".?:":
+        print("")
+        c += 1
+        while c < len(text) and text[c] == ' '
+        c += 1
+        continue
+        c += 1
