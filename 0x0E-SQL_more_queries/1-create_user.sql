8681ac0a21e19f502cae7d16be5e006a0ff6339c
bwambale03 <bwambaleedwin03@gmail.com>
2023-05-17 14:07:54 -0700

    a script that creates the MySQL server user user_0d_1.

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,10 @@
 --Script Creates the user user_0d_1 with all privileges.
+-- Script Creates the user user_0d_1 with all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
+-- Grant all privileges on *.* to user_0d_1
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
+-- Flush privileges
 FLUSH PRIVILEGES;
+
