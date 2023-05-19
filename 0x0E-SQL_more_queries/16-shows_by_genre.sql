102e3bf9bafe1f8124406f48a58b71ed98a93f33
bwambale03 <bwambaleedwin03@gmail.com>
2023-05-19 07:11:09 -0700

    a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,4 +1,4 @@
---Script Lists all shows and genres linked to the show from the
+-- Script Lists all shows and genres linked to the show from the
 -- database hbtn_0d_tvshows.
 -- Records are ordered by ascending show title and genre name.
 SELECT t.`title`, g.`name`
