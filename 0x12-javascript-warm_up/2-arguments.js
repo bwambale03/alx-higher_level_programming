9678257d1bab620c40f737458e39b1b8d5766fc6
bwambale03 <bwambaleedwin03@gmail.com>
2023-06-14 16:37:02 -0700

    a script that prints a message depending of the number of arguments passedgit push

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,3 @@
 #!/usr/bin/node
-const argsCount = process.argv.length - 2;
-
-if (argsCount === 0){
-	console.log('No  Argument');
-}else if (argsCount === 1){
-	console.log('Aergument Found');
-} else {
-	console.log('Arguments Found');
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
