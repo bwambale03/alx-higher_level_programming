c58eb4d1275770d6f9ca7ff07b90d7463ebea0b4
bwambale03 <bwambaleedwin03@gmail.com>
2023-06-15 14:36:58 -0700

    a function that executes x times a function.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
+
 function executeXtimes(x, theFunction) {
 	for (let i = 0; i <= x; i++) {
 		theFunction();
 	}
 }
-function Uganda() {
-	console.log('Rich in Natural Resources');
+
+function printMessage() {
+	console.log('Executing theFunction');
 }
-executeXtimes(3, Uganda);
+
+executeXtimes(3, printMessage);
