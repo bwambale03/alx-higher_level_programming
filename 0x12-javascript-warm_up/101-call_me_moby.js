65cb7daf682c37994ab530bfb89d080a860956e8
bwambale03 <bwambaleedwin03@gmail.com>
2023-06-15 21:53:17 -0700

    a function that executes x times a function.

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-exports.callMeMoby = function(x, theFunction) {
+/*exports.callMeMoby = function(x, theFunction) {
 	for (let i = 0; i < x; i++) theFunction();
 };
 
@@ -9,3 +9,16 @@ function printFunction(){
 }
 
 exports.callMeMoby(3, printFunction);
+*/
+
+function executeXtimes(x, theFunction) {
+	for (let i = 0; i < x; i++) {
+		theFunction();
+	}
+}
+
+function printFunction() {
+	console.log('Executing theFunction');
+}
+
+executeXtimes(5, printFunction);
