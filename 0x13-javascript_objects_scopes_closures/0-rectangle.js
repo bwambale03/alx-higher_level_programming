65867d7a727138ffdd5e58ee432a26fd385563a6
bwambale03 <bwambaleedwin03@gmail.com>
2023-06-21 15:02:48 -0700

    an empty class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,19 +1,5 @@
 #!/usr/bin/env node
 class Rectangle {
-  constructor (width, length) {
-    this.width = width;
-    this.length = length;
-  }
-
-  getArea () {
-    return this.width * this.length;
-  }
-
-  getPerimeter () {
-    return 2 * (this.length + this.width);
-  }
+  // Empty constructor
 }
-const rect = new Rectangle(4, 8);
-console.log(rect.getArea());
-
-console.log(rect.getPerimeter());
+module.exports = Rectangle;
