9f9d6db0b04baaf9f9a575b6fb9ff965370dac15
bwambale03 <bwambaleedwin03@gmail.com>
2023-06-21 16:36:52 -0700

    a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@ class Rectangle {
     this.width = w;
     this.height = h;
 
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+    if (w === 0 || h === 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       // return an empty object
       return {};
     }
