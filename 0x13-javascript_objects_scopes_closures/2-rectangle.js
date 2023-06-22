4de541ef277529c95c67016962b59f752a4bf27f
bwambale03 <bwambaleedwin03@gmail.com>
2023-06-21 17:23:59 -0700

    a class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,22 +1,10 @@
-#!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
     if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
       this.width = w;
       this.height = h;
     } else {
-      this.width = 0;
-      this.height = 0;
-    }
-  }
-
-  print () {
-    if (this.width === 0 || this.height === 0) {
-      console.log('Empty rectangle');
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
+      return {}; // Create an empty object if w or h is invalid
     }
   }
 }
