583574c54a22289fb7baa9315689ef26bbc28580
bwambale03 <bwambaleedwin03@gmail.com>
2023-06-30 19:32:42 -0700

    a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with "You got me!"
 
-
 curl -sLX PUT -d "user_id=98" -H "Origin: HolbertonSchool" -H "Content-Type: application/x-www-form-urlencoded" -H "Referer: http://0.0.0.0:5000/catch_me" "http://0.0.0.0:5000/catch_me"
