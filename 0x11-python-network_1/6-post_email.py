95e4d7b6d79afe0a5491772d03087999c339f14d
bwambale03 <bwambaleedwin03@gmail.com>
2023-07-01 13:10:16 -0700

    a Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
-import sys
-import requests
-url = sys.argv[1]
-email = sys.argv[2]
 
-data = {'email': email}
-response = requests.post(url, data=data)
-print('your email is : ', email)
-print(response.text)
+"""
+This script takes a URL and an email address as command-line arguments,
+sends a POST request to the URL with the email as a parameter, and displays
+the body of the response.
+
+Usage: ./6-post_email.py <URL> <email>
+
+Example: ./6-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
+"""
+
+import requests
+import sys
+
+def post_email(url, email):
+    """Sends a POST request to the specified URL with the email as a parameter."""
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    print("Your email is: ", email)
+    print(response.text)
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    post_email(url, email)
