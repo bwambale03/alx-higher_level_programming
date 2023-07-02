e708a26202a1a2554b224b16b7ed2a5194794591
bwambale03 <bwambaleedwin03@gmail.com>
2023-07-01 17:15:26 -0700

    a Python script that takes 2 arguments in order to solve the challenge: The Holberton School staff evaluates candidates applying for a back-end position with multiple technical challenges, like this one:Please list 10 commits (from the most recent to oldest) of the repository “rails” by the user “rails” You must use the GitHub API, here is the documentation https://developer.github.com/v3/repos/commits/ Print all commits by:  (one by line): the challenge | The first argument will be the repository name, The second argument will be the owner name, You must use the packages requests and sys, You are not allowed to import packages other than requests and sys, You don’t need to check arguments passed to the script (number or type), supprisingly Only 17% of applicants for a backend position at Holberton finished this task in less than 15 minutes.

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+A script that lists the 10 most recent commits on a given GitHub repository.
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    # Construct the API URL using the provided repository and owner names
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    url = f"https: // api.github.com / repos / {owner_name} / {rep" \
+        " ository_name} / commits"
+
+    # Send a GET request to the GitHub API
+    response = requests.get(url)
+
+    # Parse the JSON response into a list of commits
+    commits = response.json()
+
+    try:
+        # Loop through the first 10 commits (or fewer if available)
+        for commit in commits[:10]:
+            # Extract the commit SHA and author name
+            commit_sha = commit["sha"]
+            author_name = commit["commit"]["author"]["name"]
+
+            # Print the commit SHA and author name
+            print(f"{commit_sha}: {author_name}")
+    except IndexError:
+        pass
