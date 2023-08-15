93e701418d73fd22aedaad720732f5e61668cf5f
Edwin Bwambale <bwambaleedin03@gmail.com>
2023-08-15 15:21:54 -0700

    a script that lists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,23 +1,33 @@
 #!/usr/bin/python3
 """
-Changes the name of the State object with id = 2 to
-New Mexico in the database hbtn_0e_6_usa.
-Usage: ./12-model_state_update_id_2.py <mysql username> /
-                                       <mysql password> /
-                                       <database name>
+Lists all State objects, and corresponding City objects,
+contained in the database hbtn_0e_101_usa.
+Usage: ./101-relationship_states_cities_list.py <mysql username> /
+                                                <mysql password> /
+                                                <database name>
 """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
+from relationship_state import State, Base
+from relationship_city import City
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}",
                            pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter_by(id=2).first()
-    state.name = "New Mexico"
-    session.commit()
+    states = session.query(State).order_by(State.id).all()
+
+    for state in states:
+        print(f"{state.id}: {state.name}")
+        for city in state.cities:
+            print(f"    {city.id}: {city.name}")
+
+    session.close()
