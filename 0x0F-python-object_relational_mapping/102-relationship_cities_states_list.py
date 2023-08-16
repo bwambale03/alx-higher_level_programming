3faf6126ebf995a4764fa578e3b24d0d17e8cee3
Edwin Bwambale <bwambaleedin03@gmail.com>
2023-08-15 23:36:25 -0700

    a script that lists all City objects from the database hbtn_0e_101_usa

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -8,27 +8,15 @@ Usage: ./102-relationship_cities_states_list.py <mysql username> /
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
+from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    engine = create_engine(f"mysql+mysqldb://{username}:{password}@localhost:3306/{database_name}",
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City).order_by(City.id).all()
-
-    for city in cities:
-        state = session.query(State).filter(State.id == city.state_id).first()
-        print(f"{city.id}: {city.name} -> {state.name}")
-
-    session.close()
-
+    for city in session.query(City).order_by(City.id):
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
