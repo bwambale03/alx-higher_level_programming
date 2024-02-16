99a4d6121d154be0fc872e1e2f8c8d74740db244
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-02-16 01:45:53 -0500

    updated 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,29 @@
-#!/usr/nin/python3
-"""defines a text function indentation"""
+#!/usr/bin/python3
+"""Defines a text-indentation function."""
 
 
 def text_indentation(text):
-    """print text with two new lines after each '.', '?' and ':'.
+    """Print text with two new lines after each '.', '?', and ':'.
 
     Args:
-    text(string): The text to print.
+        text (string): The text to print.
     Raises:
-    TypeError: If text is not a string.
+        TypeError: If text is not a string.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     c = 0
-    while c < len(text and text[c] == ' '):
-            c += 1
+    while c < len(text) and text[c] == ' ':
+        c += 1
 
     while c < len(text):
         print(text[c], end="")
-        if(text[c] == "\n" or text[c] in ".?:"):
+        if text[c] == "\n" or text[c] in ".?:":
             if text[c] in ".?:":
-                print("")
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
                 c += 1
-                while c < len(text) and text[c] == ' ':
-                    c += 1
-                continue
-                c += 1
+            continue
+        c += 1
