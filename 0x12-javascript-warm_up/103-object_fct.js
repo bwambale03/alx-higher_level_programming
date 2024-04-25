206dfc042906744da642d4cf5d66f68076dc6fc5
Edwin Bwambale <bwambaleedwin03@gmail.com>
2024-04-25 05:28:39 -0400

    Updated the script 103-object_fct.js by adding a new function incr that increments the integer value.

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 const myObject = {
-  type: 'object',
-  value: 12,
-  incr: function() {
-    this.value += 1;
-  }
+	type: 'object',
+	value: 12,
+	incr: function(){
+		this.value += 1;
+	}
 };
+console.log(myObject);
 
-console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
-console.log(myObject);
+console.log(myObject)
